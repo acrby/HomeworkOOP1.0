96f2e2aa0f9b8e92bd00dc3b51a861f72ccedf41
D:\Repo <hudkou@hotmail.com>
2021-09-30 15:59:39 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HomeworkOOP1._0
+{
+
+    class MyClass
+    {
+       public int fieldOfClass;
+
+        public void Method ()
+        {
+            Console.WriteLine(fieldOfClass);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass a = new MyClass();
+            a.fieldOfClass = 1;
+            a.Method();
+
+            MyClass b = new MyClass();
+            b.fieldOfClass = 2;
+            b.Method();
+
+            Console.ReadKey();
+        }
+    }
+}
